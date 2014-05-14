a127a5760e8481cc6afa5af5d677eb53049c0eb0
Sergeo17 <sergio.umana@gmail.com>
2014-05-13 20:36:25 -0700

    final changes before pushing live

--- app/mailers/rsvp_mailer.rb
+++ app/mailers/rsvp_mailer.rb
@@ -1,14 +1,9 @@
 class RsvpMailer < ActionMailer::Base
   default :from => "ashleyandsergio@gmail.com"
 
-  email1 = "sergio.umana@gmail.com"
-  email2 = "ashley.nolting@gmail.com"
-
-
-
   def rsvp_confirmation(household)
     @household = household
 
-    mail(:to => "sergio.umana@gmail.com", :cc => "ashley.nolting@gmail.com", :subject => "RSVP Confirmation")
+    mail(:to => "sergio.umana@gmail.com", :subject => "RSVP Confirmation")
   end
 end
